8183bad99af5eccaaaffbd52f07b167ff1657e7e
Torubiri Walter <toruwalt1997@gmail.com>
2023-08-10 18:55:59 -0700

    Printf  Task 2

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - The is the function where all codes run
+ * Return: it has to return 0 else error
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
