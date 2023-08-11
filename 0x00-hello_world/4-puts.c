040a524cc00502c0e2975e85e78a780cf4ebc79d
Torubiri Walter <toruwalt1997@gmail.com>
2023-08-10 18:30:55 -0700

    Puts task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - The is the function where all codes run
+ *
+ * Return: it has to return 0 else error 
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
