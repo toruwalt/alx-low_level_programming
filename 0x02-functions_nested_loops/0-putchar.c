79ed2df3b7f3806e9ba846579d795f7218c6b26a
Torubiri Walter <toruwalt1997@gmail.com>
2023-08-15 06:06:32 -0700

    Task 1 updated

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
  *main - The code body
  *
@@ -8,6 +7,14 @@
 
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
