abbe57201a84fefbe8a746f6d1ba4f79819d2dfb
Torubiri Walter <toruwalt1997@gmail.com>
2023-08-14 19:30:45 -0700

    Task 4 completed

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+/**
+ *main - The blanket
+ *Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
