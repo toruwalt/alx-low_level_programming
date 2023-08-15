d18f6e4a9895d279b6eb5bd9ecfe403f9245fccd
Torubiri Walter <toruwalt1997@gmail.com>
2023-08-15 07:45:05 -0700

    TAsk 2

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	void print_alphabet(void)
+	{
+		int ch;
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar('ch');
+		}
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,2 @@
 int _putchar(char n);
+void print_alphabet(void);
