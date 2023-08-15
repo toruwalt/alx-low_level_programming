9d9711789205399d489d98455febaf2193f89027
Torubiri Walter <toruwalt1997@gmail.com>
2023-08-14 19:54:29 -0700

    TAsk 6 Completed

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+/**
+ *main - The blanket
+ *Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
