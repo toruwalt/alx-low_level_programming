eb8ef38f5a0313e04e4ecd9be2e49eb204f09792
Torubiri Walter <toruwalt1997@gmail.com>
2023-08-15 05:24:08 -0700

    TAsk 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - The code body
+ *
+ *Return: can be 0 or 1
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is the description of the Function and Nested Loop Directory in the Low Level Programming repository
