aa307dee6e66be61151c438bc987853d346be28f
Torubiri Walter <toruwalt1997@gmail.com>
2023-08-17 07:19:15 -0700

    Task 3 completed

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies 2 numbers together
+ *
+ * @a: The first value that will be passed
+ *
+ * @b: The second value that will be passed
+ *
+ * Return: the product of multiplication
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
