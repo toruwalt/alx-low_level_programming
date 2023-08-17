ff095bb91e71566dd0d5e3181a9b498f90805039
Torubiri Walter <toruwalt1997@gmail.com>
2023-08-17 16:07:09 -0700

    Task 9 Re-updated

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,14 +9,14 @@
  */
 void print_square(int size)
 {
-	int i, j;
-
 	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
+		int i, j;
+
 		for (i = 1; i <= size; i++)
 		{
 			for (j = 1; j <= size; j++)
