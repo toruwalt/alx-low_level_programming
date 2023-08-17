8cf95bab8875a92ec34bebd7196554b1205cdf5b
Torubiri Walter <toruwalt1997@gmail.com>
2023-08-16 20:28:47 -0700

    Task 11 completed

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,14 +1,15 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * add - adds 2 numbers together
- * @n: the int to extract the last digit from
+ * @p: first digit to be added
+ * @q: second digit to be added
  * Return: value of the last digit
  */
 int add(int p, int q)
 {
 	int i;
-	
+
 	i = p + q;
 	return (i);
 }
