9e1e20100984a82ec54b28c7ef8ca81b726a6a68
Torubiri Walter <toruwalt1997@gmail.com>
2023-08-17 12:10:06 -0700

    Task 4 completed

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - print numbers
+ *
+ * Return: 0 always
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
