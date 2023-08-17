6a9e179aa1e81e3d20a0810f6d9152533c078930
Torubiri Walter <toruwalt1997@gmail.com>
2023-08-17 15:58:46 -0700

    Task 9 completed

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_square - print squares according to number inputted.
+ *
+ * @size: Number of lines of squares is printed
+ * Return: size
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size == 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
