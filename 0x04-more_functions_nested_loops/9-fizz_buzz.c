8610e2e94d405b16c9169cf634b1936dfd799830
Torubiri Walter <toruwalt1997@gmail.com>
2023-08-19 12:06:18 -0700

    TAsk 9 added after deadline

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main: Run the FizzBuzz Program
+ *
+ *Return: 0 always
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			if (i % 5 == 0)
+			{
+				printf("FizzBuzz ");
+			}
+			else
+			{
+				printf("Fizz ");
+			}
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%i ", i);
+		}
+	}
+	return (0);
+}
