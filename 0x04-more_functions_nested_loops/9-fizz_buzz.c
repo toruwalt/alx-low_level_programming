30c1cf4d6d67a1a4508d2b890c3dfbd8070a5d95
Torubiri Walter <toruwalt1997@gmail.com>
2023-08-19 12:13:59 -0700

    TAsk 9 added after deadline

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -26,7 +26,14 @@ int main(void)
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			if (i % 3 == 0)
+                        {
+                                printf("FizzBuzz ");
+                        }
+                        else
+                        {
+                                printf("Buzz ");
+                        }
 		}
 		else
 		{
