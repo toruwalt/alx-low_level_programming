8aedf474eb46af1d6c5868c428bb6139fab120c1
Torubiri Walter <toruwalt1997@gmail.com>
2023-08-20 06:32:12 -0700

    Debugging Task 1

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - This is the entry point
+ * if - the conditional statement
+ * else if - the second conitional statement
+ * else - the last conditional statement
+ * Return: 0 to be true
+ */
+
+void positive_or_negative(int n)
+{
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+}
