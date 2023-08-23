59f6894b0994b41ad77da1c781f31dca7b79ac17
Torubiri Walter <toruwalt1997@gmail.com>
2023-08-23 15:28:09 -0700

    Betty Correction

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,13 +2,15 @@
 #include <stdio.h>
 
 /**
- *
- *
+ * _strcat - Combines 2 arrays
+ * @dest: WHere array is copied to
+ * @src: where array is copied from
+ * Return: Returns destination
  */
 char *_strcat(char *dest, char *src)
 {
 	int i, j;
-	
+
 	i = 0;
 	j =  0;
 	while (dest[i] != '\0')
