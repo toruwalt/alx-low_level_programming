acec857275a895eb44a3c46047e89846b6f3249f
Torubiri Walter <toruwalt1997@gmail.com>
2023-08-28 12:38:43 -0700

    TAsk 2 completed

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memcpy - copies n bytes from memory area src to memory area dest
+ * @dest: The destination to be populated
+ * @src: The source to populate with
+ * @n: The number of times it is populated
+ * Return: 0 always
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		src[i] = dest[i];
+	}
+	return (dest);
+}
