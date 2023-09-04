7ef11fa0dbb5ba93b2f9110429049c8c85cdb7e7
Torubiri Walter <toruwalt1997@gmail.com>
2023-09-04 15:49:29 -0700

    Malloc Task 3 completed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -24,7 +24,6 @@ char *str_concat(char *s1, char *s2)
 	{
 		s2 = "";
 	}
-	
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		lengthA++;
@@ -46,8 +45,8 @@ char *str_concat(char *s1, char *s2)
 	}
 
 	for (j = 0; s2[j] != '\0'; j++)
-        {
-                d[lengthA + j] = s2[j];
-        }
+	{
+		d[lengthA + j] = s2[j];
+	}
 	return (d);
 }
