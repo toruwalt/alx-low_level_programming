ce5266ab65e9d9ca64becb634461424b727b79a4
Torubiri Walter <toruwalt1997@gmail.com>
2023-09-04 06:09:04 -0700

    First task under Malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array: writes a function that creates an array of chars, and initializes it with a specific char.
+ * @size: checks the size
+ * @c - the string
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char* d;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return NULL;
+	}
+
+	d = (char*)malloc(size * sizeof(char));
+
+	if (d == NULL)
+	{
+		return NULL;
+	}
+	for (i = 0; i < size; i++)
+	{
+		d[i] = c;
+	}
+	return d;
+}
