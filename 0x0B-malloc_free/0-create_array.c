11398c4a95f6d3a3af6c1df080cc00fe1df9cf4d
Torubiri Walter <toruwalt1997@gmail.com>
2023-09-04 06:21:36 -0700

    Malloc Task 1 updated

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,30 +3,32 @@
 #include <stdlib.h>
 
 /**
- * create_array: writes a function that creates an array of chars, and initializes it with a specific char.
+ * create_array - writes a function that creates an array of chars,
+ * and initializes it with a specific char.
  * @size: checks the size
- * @c - the string
+ * @c: the string
+ * Return: Returns the string
  */
 
 char *create_array(unsigned int size, char c)
 {
-	char* d;
+	char *d;
 	unsigned int i;
 
 	if (size == 0)
 	{
-		return NULL;
+		return (NULL);
 	}
 
-	d = (char*)malloc(size * sizeof(char));
+	d = (char *)malloc(size * sizeof(char));
 
 	if (d == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 	for (i = 0; i < size; i++)
 	{
 		d[i] = c;
 	}
-	return d;
+	return (d);
 }
