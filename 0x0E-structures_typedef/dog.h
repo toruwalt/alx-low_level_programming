21e849e87007b91b56aa617f39f945d53f55a5e4
Torubiri Walter <toruwalt1997@gmail.com>
2023-09-11 07:44:37 -0700

    Task 1 sorted

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,14 @@
  * @age: AG
  * @owner: Owner
  */
+
 typedef struct dog
 {
 	char *name;
 	float age;
 	char  *owner;
 } dog, dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 
 #endif
