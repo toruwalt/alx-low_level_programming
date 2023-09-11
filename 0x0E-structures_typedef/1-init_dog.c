fec60ed794b79d5df233da0516111cea27b7828e
Torubiri Walter <toruwalt1997@gmail.com>
2023-09-11 06:10:21 -0700

    Struct Task 0 and 3

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ *  init_dog -- a function that initialize a variable of type struct dog
+ *  @dog:  Location
+ *  @name: Name
+ *  @age: age
+ *  @owner: Owner
+ *  Return: 0 always
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	dog romeo;
+	romeo.name = name;
+	romeo.age = age;
+	romeo.owner = owner;
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+typedef struct dog{
+	char *name;
+	float age;
+	char  *owner;
+}dog, dog_t;
