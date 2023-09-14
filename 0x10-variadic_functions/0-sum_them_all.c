e4e8c48a58e6135764fe3e84d0904ac1e5fc54b9
Torubiri Walter <toruwalt1997@gmail.com>
2023-09-14 09:26:32 -0700

    Variadic Function Task 1 completed

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -5,6 +5,7 @@
 /**
  * sum_them_all - Write a function that returns the sum of all its parameters.
  * @n: The number of arguments passed
+ * Return: The total addition is returned
  */
 
 int sum_them_all(const unsigned int n, ...)
@@ -12,11 +13,18 @@ int sum_them_all(const unsigned int n, ...)
 	int sum;
 	unsigned int i;
 	va_list args;
+
 	va_start(args, n);
 	sum = 0;
+
+	if (n == 0)
+	{
+		return (0);
+	}
 	for (i = 0; i < n; i++)
 	{
 		int x = va_arg(args, int);
+
 		sum = sum + x;
 	}
 	return (sum);
