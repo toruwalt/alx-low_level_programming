fd210446359ac6c65de269826f7ec1b589ed8053
toruwalt <toruwalt@gmail.com>
2023-10-02 13:41:07 -0700

    ARgC&ArgV Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - The main function
+ * @argc: Number of arguments
+ * @argv: List of Arguments
+ * Return: 0 always
+ */
+int main(int argc, char *argv[])
+{
+	int i, j;
+
+	j = 0;
+	if (argc != 3)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			j = j + atoi(argv[i]);
+		}
+		printf("%d\n", j);
+	}
+	return (0);
+}
