5d441e6691aa8a3adeb9e12d7b2dfea31e076b4e
Torubiri Walter <toruwalt1997@gmail.com>
2023-10-02 20:15:41 +0100

    ArgV&ArgC Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - The main function
+ * @argc: Number of arguments
+ * @argv: List of Arguments
+ * Return: 0 always
+ */
+int main(int argc, char *argv[])
+{
+	int i, j;
+
+	j = 1;
+	if (argc != 3)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			j = j * atoi(argv[i]);
+		}
+		printf("%d\n", j);
+	}
+	return (0);
+}
