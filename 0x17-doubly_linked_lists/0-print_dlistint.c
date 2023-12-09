2ef0fab746836e059a53475fe2ad3007c9c9fa24
Torubiri Walter <toruwalt1997@gmail.com>
2023-12-09 00:25:06 +0000

    DLL Task0 Added README, list header

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "list.h"
+
+/*
+ * print_dlistint - Prints all items in doubly linked list
+ * @h - Head of the list
+ */
+
+size_t print_dlistint(const dlistint_t *h);
