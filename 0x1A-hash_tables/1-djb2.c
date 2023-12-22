0c7578fe64ee6eabb6726c7cbfea42055fbdf296
Torubiri Walter <toruwalt1997@gmail.com>
2023-12-22 20:46:50 +0000

    C - HashTable Task 1 Made BettyCorrection

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,14 +1,19 @@
 #include "hash_tables.h"
-
+/**
+ * hash_djb2 - Function that returns the hash of each item in hashtable
+ * @str: The character inputted
+ * Return: The hash
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long int hash;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	hash = 5381;
+
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c;
+	}
+	return (hash);
 }
