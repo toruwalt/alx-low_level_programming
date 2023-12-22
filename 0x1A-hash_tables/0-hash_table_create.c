b9f631d5b3883ce9e6c22b1d4149de22f801e9af
Torubiri Walter <toruwalt1997@gmail.com>
2023-12-22 19:13:08 +0000

    C - HashTable Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,13 +1,15 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_t *hash_table_create - A function that creates a hashtable
+ * hash_table_create - A function that creates a hashtable
  * @size: The number of items to be put into HashTable
+ * Return: The hashtable is returned
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hashTable = malloc(sizeof(hash_table_t));
+
 	hashTable->size = size;
 	hashTable->array = calloc(size, sizeof(hash_table_t));
 	return hashTable;
