d01216dc583db83f9061502eecb00bccb62098e3
Torubiri Walter <toruwalt1997@gmail.com>
2023-12-22 20:27:05 +0000

    C - HashTable Task 0 Correction

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	hashTable->size = size;
-	hashTable->array = calloc(size, sizeof(hash_table_t));
+	hashTable->array = calloc(size, sizeof(hash_node_t));
 	if (hashTable->array == NULL)
 	{
 		return (NULL);
