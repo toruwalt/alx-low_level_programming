22ca8c5f21a7a0cf2ed6ebe271e8dd44b3723a0b
Torubiri Walter <toruwalt1997@gmail.com>
2024-05-08 18:49:06 +0100

    Search Algo - Task 102:Fixed Betty

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -3,7 +3,7 @@
 #include "search_algos.h"
 
 /**
- * interpolation_search - A function that searches for a value in a 
+ * interpolation_search - A function that searches for a value in a
  * sorted array of integers using INTERPOLATION search algorithm.
  * @array: pointer to the first element of the array
  * @size: number of elements in array
@@ -13,21 +13,21 @@
 
 int interpolation_search(int *array, size_t size, int value)
 {
+	int xyz;
 	size_t pos;
 	size_t low = 0;
 	size_t high = size - 1;
 
 	if (array == NULL || size == 0)
-	{
 		return (-1);
-	}
 
 	while (low <= high)
 	{
-		pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+		xyz = (double)(high - low)
+		pos = low + (((xyz / (array[high] - array[low])) * (value - array[low]));
 		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 		if ((array[pos]) == value)
-			return pos;
+			return (pos);
 		if (array[pos] < value)
 			low = pos + 1;
 		if (array[pos] == value)
